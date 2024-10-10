5555d237e6a1b459ed16cff0a038be4127164997
Domgoz <domasgos7@gmail.com>
2024-10-10 16:21:37 +0200

    Oppgave 1 Ferdig

--- src/no/hvl/dat100ptc/oppgave1/Main.java
+++ src/no/hvl/dat100ptc/oppgave1/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		// TODO
-		
+		GPSPoint gpsPoint = new GPSPoint(1, 2.0, 3.0, 5.0);
+		System.out.println("før endring tid: " + gpsPoint.getTime());
+		gpsPoint.setTime(2);
+		System.out.print(gpsPoint.toString());
 	}
 
 }
